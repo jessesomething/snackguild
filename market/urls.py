3f0e1a5c8fa94fc5cb8121adcac352e821d6db16
Jesse <jessespalding@gmail.com>
2016-04-25 18:50:35 -0500

    Can now view trade details and ordered list in table

--- market/urls.py
+++ market/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.market_list, name='market_list')
+    url(r'^$', views.market_list, name='market_list'),
+    url(r'^market/(?P<pk>\d+)/$', views.trade_detail, name='trade_detail'),
 ]
--- market/views.py
+++ market/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Market
 
 def market_list(request):
     trades = Market.objects.filter(post_date__lte=timezone.now()).order_by('post_date')
     return render(request, 'market/market_list.html', {'trades' : trades})
+
+def trade_detail(request, pk):
+    trade = get_object_or_404(Market, pk=pk)
+    return render(request, 'market/trade_detail.html', {'trade' : trade})
